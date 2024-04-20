c034bd2fa2260d76784b1834c5e410c5969b7f2e
Anna K <a.kuchebo@mail.ru>
2024-04-20 15:23:02 +0300

    add api.go

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,13 +1,35 @@
 package api
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitHandlers(r *gin.Engine) {
 	r.GET("/status", Status)
+	r.POST("/video", uploadVideo)
 }
 
 func Status(c *gin.Context) {
 	c.JSON(200, gin.H{"name": "hackaton-mephi-2024_backend", "status": "working"})
+}
+
+func uploadVideo(c *gin.Context) {
+	const op = "uploadVideo"
+
+	file, err := c.FormFile("files")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "from": op})
+		return
+	}
+
+	fileExtension := strings.ToLower(file.Filename[strings.LastIndex(file.Filename, ".")+1:])
+
+	if fileExtension != "mp4" && fileExtension != "mp3" && fileExtension != "wav" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Use obly mp3, mp4 or wav extention"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "File uploaded"})
 }
