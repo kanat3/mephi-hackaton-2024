8505ed29efeccfb6aac03269febc49454c796a66
Anna K <a.kuchebo@mail.ru>
2024-04-20 11:41:08 +0300

    add config cors

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/api"
 	"backend/internal/config"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,7 +20,11 @@ func main() {
 	_ = cfg
 
 	// init router
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	r := gin.Default()
+	r.Use(cors.New(config))
 	api.InitHandlers(r)
 
 	// run server
