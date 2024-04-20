39646d40e053cddd543b589aa74bb37debe244f8
nkataeva <kam310503@gmail.com>
2024-04-20 11:30:47 +0300

    test get data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const req = await fetch("http://localhost:8081/status");
+      const data = await req.json();
+      console.log("data", data);
+      return data;
+    }
+
+    setData(getData());
+  }, []);
 
   return (
     <>
@@ -24,6 +36,11 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        { data.map(x => {
+          <p style={{ background: "blue", color: "red"}}>{ x.name }</p>
+        }) }
+        <p>{ data.name }</p>
+        <p>{ data.status }</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
